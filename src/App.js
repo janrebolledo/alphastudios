e2b27a1772f4b8e1d3844b6f152b6e5ecf37c3de
janrebolledo <janrebolledo63@gmail.com>
2021-12-23 23:38:03 -0800

    Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Work from "./pages/Work";
+import About from "./pages/About";
+import Shop from "./pages/Shop";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
+    <Router>
+      <Header />
       <div className="App">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/work" element={<Work />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/shop" element={<Shop />} />
+          <Route exact path="/contact" element={<Contact />} />
+        </Routes>
       </div>
+      <Footer />
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,29 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import { Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 
-import Home from "./pages/Home";
-import Work from "./pages/Work";
-import About from "./pages/About";
-import Shop from "./pages/Shop";
-import Contact from "./pages/Contact";
-
-ReactDOM.render(
-  <BrowserRouter>
-    <Header />
-    <App />
-    <Routes>
-      <Route exact path="/" element={<Home />} />
-      <Route exact path="/work" element={<Work />} />
-      <Route exact path="/about" element={<About />} />
-      <Route exact path="/shop" element={<Shop />} />
-      <Route exact path="/contact" element={<Contact />} />
-    </Routes>
-    <Footer />
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
