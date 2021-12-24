54bc4bc521cd7b144c537d93b2b9028e0ce29007
janrebolledo <janrebolledo63@gmail.com>
2021-12-23 23:32:04 -0800

    React Router Changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import "./App.css";
-import Main from "./components/Main";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 
 function App() {
   return (
       <div className="App">
-        <Header />
-        <Main />
-        <Footer />
       </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,28 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+import Home from "./pages/Home";
+import Work from "./pages/Work";
+import About from "./pages/About";
+import Shop from "./pages/Shop";
+import Contact from "./pages/Contact";
 
 ReactDOM.render(
   <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <Header />
+    <App />
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/work" element={<Work />} />
+      <Route exact path="/about" element={<About />} />
+      <Route exact path="/shop" element={<Shop />} />
+      <Route exact path="/contact" element={<Contact />} />
+    </Routes>
+    <Footer />
   </BrowserRouter>,
   document.getElementById("root")
 );
